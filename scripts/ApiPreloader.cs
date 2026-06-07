c6226119f401b145305ad65b2dd6ed212bb1223c
thefirey33 <mmethefirey@gmail.com>
2026-06-07 15:50:51 +0300

    Fix Threading Issues.

--- scripts/ApiPreloader.cs
+++ scripts/ApiPreloader.cs
@@ -30,80 +30,82 @@ public partial class ApiPreloader : Node2D
 	private int _downloadTotalCount;
 	
 
-	public override void _EnterTree()
+	public override async void _EnterTree()
 	{
-		NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Uncompressing);
-		
-		// First, if the assets already exist, uncompress them.
-		// Compression is done by the game to eliminate cluttery files.
-		if (File.Exists(NikoStorageManager.ZipFilePath))
-			NikoStorageManager.UncompressAllAssets();
-		
-		NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Preloading);
-		// Import all the current existing assets.
-		NikoStorageManager.ImportBeforeAssets();
-		
-		NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.DownloadingAndSaving);
-		Task.Run(async () =>
+		try
 		{
-			NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Connecting);
-			// Check if the NikoDex API is healthy.
-			// Check if the API's not dead.
-			var response = await NikoDexApi.RequestFromApiString($"{NikoDexApi.NikoDexOrigin}/ping");
-			
-			NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Healthy);
-			if (response == null)
+			NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Uncompressing);
+		
+			// First, if the assets already exist, uncompress them.
+			// Compression is done by the game to eliminate cluttery files.
+			if (File.Exists(NikoStorageManager.ZipFilePath))
+				NikoStorageManager.UncompressAllAssets();
+		
+			NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Preloading);
+			// Import all the current existing assets.
+			NikoStorageManager.ImportBeforeAssets();
+		
+			NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.DownloadingAndSaving);
+			await Task.Run(async () =>
 			{
-				GD.PushWarning("API is dead, switching to offline mode...");
-				NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Unhealthy);
-			}
-				
-			// Set the amount of Noiks currently available to fetch through in the API.
-			var apiCountResponse = await NikoDexApi.RequestFromApiString($"{NikoDexApi.NikoDexOrigin}/data/count");
+				NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Connecting);
+				// Check if the NikoDex API is healthy.
+				// Check if the API's not dead.
+				var response = await NikoDexApi.RequestFromApiString($"{NikoDexApi.NikoDexOrigin}/ping");
 			
-			if (apiCountResponse == null)
-			{
-				GD.PushWarning("Could not fetch API count, skipping.");
-				return;
-			}
-
-			var nikodexNoikCountRequest = int.Parse(apiCountResponse);
-			NikoDexApi.SetNikoCount(nikodexNoikCountRequest);
-			GD.Print($"Total amount of Nikos that can be downloaded: {nikodexNoikCountRequest}, PERCENTAGE: {MaxDownloadPercentage}");
-
-			NikoDexApi.ClearNikoList();
-			_downloadTotalCount = (NikoDexApi.GetNikoCount() - NikoDexApi.GetNikoLocalDb()) / MaxDownloadPercentage;
-			
-			
-			for (var i = 0; i <= _downloadTotalCount; i++)
-			{
-				int nikoId;
-				
-				// Select a random Niko ID to select from the NikoDex DB.
-				do
+				NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Healthy);
+				if (response == null)
 				{
-					nikoId = (_random.Next() % NikoDexApi.GetNikoCount()) + 1;
-				} while (NikoDexApi.DoesNikoExistInList(nikoId));
+					GD.PushWarning("API is dead, switching to offline mode...");
+					NikoDexApi.SetConnectionState(NikoDexApi.ApiConnectionState.Unhealthy);
+				}
 				
-				// Download the specified Niko data and store in the local storage.
-				var downloadedNikoData = await NikoDexApi.DownloadNikoData(nikoId);
-				if (!downloadedNikoData.HasValue) continue;
-				
-				var nikoData = downloadedNikoData.Value;
-				NikoDexApi.AddNoikToList(nikoData);
-				NikoStorageManager.StoreNiko(nikoData);
-			}
+				// Set the amount of Noiks currently available to fetch through in the API.
+				var apiCountResponse = await NikoDexApi.RequestFromApiString($"{NikoDexApi.NikoDexOrigin}/data/count");
 			
-			NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Compressing);
-			NikoStorageManager.CompressAllAssets();
+				if (apiCountResponse == null)
+				{
+					GD.PushWarning("Could not fetch API count, skipping.");
+					return;
+				}
+
+				var nikodexNoikCountRequest = int.Parse(apiCountResponse);
+				NikoDexApi.SetNikoCount(nikodexNoikCountRequest);
+				GD.Print($"Total amount of Nikos that can be downloaded: {nikodexNoikCountRequest}, PERCENTAGE: {MaxDownloadPercentage}");
+
+				NikoDexApi.ClearNikoList();
+				_downloadTotalCount = (NikoDexApi.GetNikoCount() - NikoDexApi.GetNikoLocalDb()) / MaxDownloadPercentage;
+			
+			
+				for (var i = 0; i <= _downloadTotalCount; i++)
+				{
+					int nikoId;
+				
+					// Select a random Niko ID to select from the NikoDex DB.
+					do
+					{
+						nikoId = (_random.Next() % NikoDexApi.GetNikoCount()) + 1;
+					} while (NikoDexApi.DoesNikoExistInList(nikoId));
+				
+					// Download the specified Niko data and store in the local storage.
+					var downloadedNikoData = await NikoDexApi.DownloadNikoData(nikoId);
+					if (!downloadedNikoData.HasValue) continue;
+				
+					var nikoData = downloadedNikoData.Value;
+					NikoDexApi.AddNoikToList(nikoData);
+					NikoStorageManager.StoreNiko(nikoData);
+				}
+			
+				NikoStorageManager.SetLoaderState(NikoStorageManager.GameLoaderState.Compressing);
+				NikoStorageManager.CompressAllAssets();
+			});
 
 			GetTree().ChangeSceneToFile("res://scenes/sc_main.tscn");
-		});
-	}
-
-	public override void _Process(double delta)
-	{
-		base._Process(delta);
-		QueueRedraw();
+		}
+		catch (Exception e)
+		{
+			// Catch exception and print it to the console.
+			GD.PrintErr(e);
+		}
 	}
 }
